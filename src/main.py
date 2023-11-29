7bf797e7856d0850f776c56ef86c38610edff62b
Yuvraj Chauhan <email.yuvraj.13@gmail.com>
2023-11-29 20:49:23 +0530

    removed wireframe & added logo for window

--- src/main.py
+++ src/main.py
@@ -7,6 +7,11 @@ screen = pygame.display.set_mode((600, 600))
 
 running = True
 
+pygame.display.set_caption("Snakes and Ladders - OODP Project, Group 2")
+icon = pygame.image.load('src/img/logo.png')
+pygame.display.set_icon(icon)
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
