b7556e150ba0a39637c35570191e3b9c0c8a28ef
Yuvraj Chauhan <email.yuvraj.13@gmail.com>
2023-12-06 18:50:27 +0530

    added cell.py

--- src/board.py
+++ src/board.py
@@ -1,37 +1,73 @@
 import pygame
+from snakes_ladders.constants import WHITE, BLACK
+from cell import Cell  # Assuming your Cell class is in a module named 'cell'
+
+HEIGHT = 600
+WIDTH = 600
 
 class Board:
-    def __init__(self, rows, cols, square_size, start_x, start_y):
-        self.rows = rows
-        self.cols = cols
-        self.square_size = square_size
-        self.start_x = start_x
-        self.start_y = start_y
+    def __init__(self, x, y):
+        self.height = HEIGHT
+        self.width = WIDTH
+        self.rows = 10
+        self.cols = 10
+        self.x = x
+        self.y = y
+        self.cells = self.create_board()
 
-    def draw(self, screen):
-        screen.fill((255, 255, 255))
+    def create_board(self):
+        height = WIDTH // self.cols
         number = 101
         dir = 1
+        cells = []
 
         for i in range(self.rows):
-            if i % 2 == 0:
+            row = []
+            if (i % 2 == 0):
                 for j in range(self.cols):
                     number = number - dir
+                    position = number
+                    isFirst = isLast = False
 
-                    pygame.draw.rect(screen, (0, 0, 0), (self.start_x + j * self.square_size, self.start_y + i * self.square_size, self.square_size, self.square_size), 1)
-                    font = pygame.font.Font(None, 36)
-                    text = font.render(str(number), True, (0, 0, 0))
-                    text_rect = text.get_rect(center=(self.start_x + j * self.square_size + self.square_size // 2, self.start_y + i * self.square_size + self.square_size // 2))
-                    screen.blit(text, text_rect)
+                    if number == 1:
+                        isFirst = True
+                    elif number == 100:
+                        isLast = True
+
+                    cell_instance = Cell(position, height, isFirst, isLast)
+                    row.append(cell_instance)
+
+                    
             else:
                 for j in range(self.cols):
-                    pygame.draw.rect(screen, (0, 0, 0), (self.start_x + j * self.square_size, self.start_y + i * self.square_size, self.square_size, self.square_size), 1)
-                    font = pygame.font.Font(None, 36)
-                    text = font.render(str(number), True, (0, 0, 0))
-                    text_rect = text.get_rect(center=(self.start_x + j * self.square_size + self.square_size // 2, self.start_y + i * self.square_size + self.square_size // 2))
-                    screen.blit(text, text_rect)
+                    position = number
+                    isFirst = isLast = False
+
+                    if number == 1:
+                        isFirst = True
+                    elif number == 100:
+                        isLast = True
+
+                    cell_instance = Cell(position, height, isFirst, isLast)
+                    row.append(cell_instance)
 
                     number = number - dir
 
             number -= 10
             dir *= -1
+            cells.append(row)
+
+        return cells
+
+    def draw(self, screen):
+        screen.fill(WHITE)
+
+        for i, row in enumerate(self.cells):
+            for j, cell_instance in enumerate(row):
+                height = cell_instance.CellHeight
+                rect = pygame.Rect(self.x + j * height, self.y + i * height, height, height)
+                pygame.draw.rect(screen, (0, 0, 0), rect, 1)
+                font = pygame.font.Font(None, 36)
+                text = font.render(str(cell_instance.cellValue), True, BLACK)
+                text_rect = text.get_rect(center=(self.x + j * height + height // 2, self.y + i * height + height // 2))
+                screen.blit(text, text_rect)
---
+++ src/snakes_ladders/constants.py
@@ -0,0 +1,13 @@
+import pygame
+
+WIDTH  = 800
+HEIGHT = 640
+
+ROWS = 10
+COLS = 10
+
+SQUARE_SIZE = WIDTH // COLS
+
+#RGB Color Coding
+WHITE = (255, 255, 255)
+BLACK = (0 ,0 , 0)
