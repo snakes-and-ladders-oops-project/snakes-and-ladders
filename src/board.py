cd340e5fa06da4bc15fb7f99c899f7bc9f6f8526
Yuvraj Chauhan <email.yuvraj.13@gmail.com>
2023-12-16 17:52:36 +0530

    added jumper cell

--- src/board.py
+++ src/board.py
@@ -1,6 +1,6 @@
 import pygame
 from snakes_ladders.constants import WHITE, BLACK
-from cell import Cell  
+from jumper import JumperCell, SnakeCell, LadderCell
 
 HEIGHT = 600
 WIDTH = 600
@@ -21,6 +21,32 @@ class Board:
         dir = 1
         cells = []
 
+        snakes = [
+            (98, 78),
+            (95, 75),
+            (92, 88),
+            (83, 57),
+            (73, 15),
+            (46, 5),
+            (44, 22),
+            (33, 9),
+            (28, 12),
+            (21, 2)
+        ]
+
+        ladders = [
+            (8, 29),
+            (16, 45),
+            (26, 54),
+            (36, 89),
+            (49, 67),
+            (62, 91),
+            (64, 84),
+            (71, 93),
+            (87, 99),
+            (94, 98)
+        ]
+
         for i in range(self.rows):
             row = []
             if (i % 2 == 0):
@@ -34,10 +60,20 @@ class Board:
                     elif number == 100:
                         isLast = True
 
-                    cell_instance = Cell(position, height, isFirst, isLast)
+                    for snake in snakes:
+                        if snake[0] == number:
+                            cell_instance = SnakeCell(position, height, isFirst, isLast, jump_to=snake[1])
+                            break
+                    else:
+                        for ladder in ladders:
+                            if ladder[0] == number:
+                                cell_instance = LadderCell(position, height, isFirst, isLast, jump_to=ladder[1])
+                                break
+                        else:
+                            cell_instance = JumperCell(position, height, isFirst, isLast)
+
                     row.append(cell_instance)
 
-                    
             else:
                 for j in range(self.cols):
                     position = number
@@ -48,7 +84,18 @@ class Board:
                     elif number == 100:
                         isLast = True
 
-                    cell_instance = Cell(position, height, isFirst, isLast)
+                    for snake in snakes:
+                        if snake[0] == number:
+                            cell_instance = SnakeCell(position, height, isFirst, isLast, jump_to=snake[1])
+                            break
+                    else:
+                        for ladder in ladders:
+                            if ladder[0] == number:
+                                cell_instance = LadderCell(position, height, isFirst, isLast, jump_to=ladder[1])
+                                break
+                        else:
+                            cell_instance = JumperCell(position, height, isFirst, isLast)
+
                     row.append(cell_instance)
 
                     number = number - dir
@@ -66,8 +113,18 @@ class Board:
             for j, cell_instance in enumerate(row):
                 height = cell_instance.CellHeight
                 rect = pygame.Rect(self.x + j * height, self.y + i * height, height, height)
-                pygame.draw.rect(screen, (0, 0, 0), rect, 1)
+
+                # Fill the entire cell with the specified color
+                pygame.draw.rect(screen, cell_instance.color if hasattr(cell_instance, 'color') else (255, 255, 255), rect)
+                
+                # Draw black borders for each cell
+                pygame.draw.rect(screen, BLACK, rect, 1)
+
                 font = pygame.font.Font(None, 36)
                 text = font.render(str(cell_instance.cellValue), True, BLACK)
                 text_rect = text.get_rect(center=(self.x + j * height + height // 2, self.y + i * height + height // 2))
                 screen.blit(text, text_rect)
+
+                # Skip rendering "Jump to" message
+                if isinstance(cell_instance, JumperCell) and cell_instance.jump_to is not None:
+                    pass
--- src/player.py
+++ src/player.py
@@ -1,4 +1,5 @@
 import pygame
+from jumper import JumperCell, SnakeCell, LadderCell
 
 HEIGHT = 600
 WIDTH = 600
@@ -13,18 +14,16 @@ class Player:
         self.token_color = token_color
         self.radius = SQUARE_SIZE // 6  
 
-    
     def set_board(self, board):
         self.board = board
 
-
     def move(self, steps):
+        initial_position = self.position
         self.position += steps
 
         if self.position > COLS * COLS:  
             self.position = COLS * COLS
 
-        
         row = (self.position - 1) // COLS  
         col = (self.position - 1) % COLS
 
@@ -33,6 +32,15 @@ class Player:
 
         self.x = col * SQUARE_SIZE + SQUARE_SIZE // 2
         self.y = HEIGHT - (row * SQUARE_SIZE + SQUARE_SIZE // 2)
+
+        # Check if the current cell has a jump destination
+        current_cell = self.board.cells[COLS - 1 - row][col]
+        if isinstance(current_cell, JumperCell) and current_cell.jump_to is not None:
+            self.position = current_cell.jump_to
+
+        # Check if the player has reached the last cell
+        if self.position >= 100:
+            self.position = 100
 
     def draw_token(self, screen):
         if self.board is None:
